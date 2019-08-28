c249a182dcb2dd548459a8d5e89070746bf1067b
avicndugu <avicnduguh@gmail.com>
2019-08-28 03:48:28 +0300

    Updates

---
+++ precache-manifest.431f3032e994e63d6580434d7d00761c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca05c5f2a1847867943cf4fd8130b2c6",
+    "url": "/new-lesson/index.html"
+  },
+  {
+    "revision": "d8d37141b31e4b97437c",
+    "url": "/new-lesson/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "87887dcec93468a5a778",
+    "url": "/new-lesson/static/js/2.67aa4224.chunk.js"
+  },
+  {
+    "revision": "d8d37141b31e4b97437c",
+    "url": "/new-lesson/static/js/main.fd1b1d04.chunk.js"
+  },
+  {
+    "revision": "1ed16d9551640fda6d6f",
+    "url": "/new-lesson/static/js/runtime~main.9277d32f.js"
+  }
+]);
